5190be7044ccc8000c5a31e13e194a3306023927
William <bzfishmaster94@gmail.com>
2024-07-18 18:45:20 -0400

    Added functionality with all viewing selections, but now need to figure out adding

--- server.js
+++ server.js
@@ -58,15 +58,75 @@ const answer = await inquirer.prompt([
 
 switch (answer.choice) {
     case 'View All Employees':
-        // Could add a function here that runs the code needed
-        console.log('test');
+        viewAllEmployees();
+        break;
+    case 'Add Employee':
+        // function
+        break;
+    case 'Update Employee Role':
+        //function
+        break;
+    case 'View All Roles':
+        viewAllRoles();
+        break;
+    case 'Add Role':
+        //function
+        break;
+    case 'View All Departments':
+        viewAllDepartments();
+        break;
+    case 'Add Department':
+        //function
+        break;
+    case 'Quit':
+        pool.end();
+        console.log('Ending Program...');
+        process.exit(0); // exits the application with 0 status code.
         break;
     default:
         console.log('Invalid choice')
 }
 
 console.log(answer.choice);
-mainApp();
+}
+
+function viewAllEmployees() {
+    pool.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name,' ', manager.last_name) AS manager
+        FROM employee 
+        INNER JOIN role ON employee.role_id = role.id 
+        INNER JOIN department ON role.department_id = department.id
+        LEFT JOIN employee manager ON employee.manager_id = manager.id`, function (err, result) {
+            if (err) {
+                console.error('Error executing query:', err);
+                return;
+            }
+        console.table(result.rows);
+        mainApp();
+    })
+}
+
+function viewAllDepartments() {
+    pool.query(`SELECT department.id, department.name FROM department`, function (err, result) {
+        console.table(result.rows);
+        mainApp();
+    })
+}
+
+function viewAllRoles() {
+    pool.query(`SELECT role.id, role.title, department.name AS department, role.salary 
+    FROM role
+    INNER JOIN department ON role.department_id = department.id`, function (err, result) {
+        if (err) {
+            console.error('Error executing query:', err);
+            return;
+        }
+    console.table(result.rows);
+    mainApp();
+    })
+}
+
+function quit() {
+
 }
 
 
